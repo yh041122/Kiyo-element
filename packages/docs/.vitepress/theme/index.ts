c637aa4cf137e6b5ac338f25df1f7fdf4d458bda
yuanhao1122 <741821607@qq.com>
2026-03-12 16:09:05 +0800

    fixed: preview引入

--- packages/docs/.vitepress/theme/index.ts
+++ packages/docs/.vitepress/theme/index.ts
@@ -1,10 +1,10 @@
 import DefaultTheme from "vitepress/theme";
 import { type App } from "vue";
 import KiyoElement from "kiyo-element";
-// import { ElementPlusContainer } from "@vitepress-demo-preview/component";
-// import "@vitepress-demo-preview/component/dist/style.css";
-import { ElementPlusContainer } from "vitepress-preview-component";
-import "vitepress-preview-component/style.css";
+import { ElementPlusContainer } from "@vitepress-demo-preview/component";
+import "@vitepress-demo-preview/component/dist/style.css";
+// import { ElementPlusContainer } from "vitepress-preview-component";
+// import "vitepress-preview-component/style.css";
 import "kiyo-element/dist/index.css";
 
 export default {
